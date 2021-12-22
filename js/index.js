0a19d876fdc3db0248a5e0276d0fedb0ae2c0d75
Olegio <ogorski23@gmail.com>
2021-12-22 17:30:55 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+let slidesId = 1;
+
+showSlides(slidesId);
+
+function nextSlide() {
+    showSlides(slidesId += 1);
+}
+
+function currentSlide(n) {
+    showSlides(slidesId = n);
+}
+
+function previousSlide() {
+    showSlides(slidesId -= 1);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slider-item");
+
+    if (n > slides.length) {
+        slides = 1;
+    }
+
+    if (n < 1) {
+        slidesId = slides.length;
+    }
+
+    for (let slide of slides) {
+        slide.style.display = 'none';
+    }
+
+    slides[slidesId - 1].style.display = 'block';
+}
